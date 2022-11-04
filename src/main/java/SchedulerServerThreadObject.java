f4f74fbd6e729df8df11e0c2a3d623d55bf8107a
bhavanabojja05 <bojja97@gmail.com>
2022-11-03 23:16:42 -0600

    Committing thread execution/classes

--- src/main/java/SchedulerServerThreadObject.java
+++ src/main/java/SchedulerServerThreadObject.java
@@ -1,3 +1,4 @@
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.HttpURLConnection;
 import java.util.HashMap;
@@ -8,14 +9,58 @@ public class SchedulerServerThreadObject {
     private HashMap<String, HttpURLConnection> connMap;
     private HttpURLConnection dayWiseConn;
     private HashMap<String, Integer> casesMap;
-    private java.io.ObjectOutputStream eastus;
-    private ObjectOutputStream uksouth;
-    private ObjectOutputStream westus;
+    private java.io.ObjectOutputStream eastus1;
+    private java.io.ObjectOutputStream uksouth;
+    private java.io.ObjectOutputStream eastus2;
     private List<ServerClientProtocol> energyListLow;
     private List<ServerClientProtocol> energyListHigh;
 
+    private ObjectInputStream eastusInput;
+
+    private ObjectInputStream westusInput;
+
+    public ObjectInputStream getWestusInput() {
+        return westusInput;
+    }
+
+    public void setWestusInput(ObjectInputStream westusInput) {
+        this.westusInput = westusInput;
+    }
+
+    public ObjectInputStream getUkSouthInput() {
+        return ukSouthInput;
+    }
+
+    public void setUkSouthInput(ObjectInputStream ukSouthInput) {
+        this.ukSouthInput = ukSouthInput;
+    }
+
+    private ObjectInputStream ukSouthInput;
+
+
+
+    private int threadComplete;
+
+
+
     public SchedulerServerThreadObject(){
 
+    }
+
+    public ObjectInputStream getEastusInput() {
+        return eastusInput;
+    }
+
+    public void setEastusInput(ObjectInputStream eastusInput) {
+        this.eastusInput = eastusInput;
+    }
+
+    public void setThreadComplete(int threadComplete) {
+        this.threadComplete = threadComplete;
+    }
+
+    public int getThreadComplete() {
+        return threadComplete;
     }
 
     public void setDayWiseConn(HttpURLConnection dayWiseConn) {
@@ -39,16 +84,16 @@ public class SchedulerServerThreadObject {
         this.casesMap = casesMap;
     }
 
-    public void setEastus(ObjectOutputStream eastus) {
-        this.eastus = eastus;
+    public void setEastus1(ObjectOutputStream eastus1) {
+        this.eastus1 = eastus1;
     }
 
     public void setUksouth(ObjectOutputStream uksouth) {
         this.uksouth = uksouth;
     }
 
-    public void setWestus(ObjectOutputStream westus) {
-        this.westus = westus;
+    public void setEastus2(ObjectOutputStream eastus2) {
+        this.eastus2 = eastus2;
     }
 
     public void setEnergyListLow(List<ServerClientProtocol> energyListLow) {
@@ -67,16 +112,16 @@ public class SchedulerServerThreadObject {
         return casesMap;
     }
 
-    public ObjectOutputStream getEastus() {
-        return eastus;
+    public ObjectOutputStream getEastus1() {
+        return eastus1;
     }
 
     public ObjectOutputStream getUksouth() {
         return uksouth;
     }
 
-    public ObjectOutputStream getWestus() {
-        return westus;
+    public ObjectOutputStream getEastus2() {
+        return eastus2;
     }
 
     public List<ServerClientProtocol> getEnergyListLow() {
